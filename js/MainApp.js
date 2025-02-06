d689634d4994239d8363719848e17204f1d9b126
stlthwl <stlthwl92@mail.ru>
2025-02-06 11:20:10 +0300

    test: test tgData

--- js/MainApp.js
+++ js/MainApp.js
@@ -2,39 +2,44 @@ const { createApp } = Vue;
 
 const MainApp = {
     template: `
-          <div>
+        <div>
             <h1 v-if="error">{{ message }}</h1>
-          </div>>
-        `,
+        </div>
+    `,
     data() {
         return {
             telegramId: '',
             app: '',
-            data: '',
+            data: null,
             message: '',
             error: false,
-
         }
     },
     mounted() {
-        const urlParams = new URLSearchParams(window.location.search)
+        const urlParams = new URLSearchParams(window.location.search);
 
         this.telegramId = urlParams.get('telegram_id') || '';
         this.app = urlParams.get('app') || '';
 
         try {
-            this.data = {
+            const tgData = {
                 organization: JSON.parse(urlParams.get('organization') || '[]'),
                 projects: JSON.parse(urlParams.get('projects') || '[]'),
                 categories: JSON.parse(urlParams.get('categories') || '[]'),
                 priorities: JSON.parse(urlParams.get('priorities') || '[]')
             };
-            console.log('Это данные (JSON):', this.data);
 
-            window.tg_data = this.data
+            if (Object.values(tgData).every(arr => Array.isArray(arr))) {
+                console.log('Это данные (JSON):', tgData);
+                this.data = tgData;
+                window.tg_data = tgData
+            } else {
+                throw new Error('Некорректные данные');
+            }
         } catch (error) {
             console.error('Ошибка при парсинге JSON:', error);
-            this.data = {}; // Или другое значение по умолчанию, если произошла ошибка
+            this.message = 'Ошибка загрузки данных';
+            this.error = true;
         }
     },
     methods: {
@@ -42,4 +47,4 @@ const MainApp = {
     }
 };
 
-createApp(MainApp).mount('#main_app')
+createApp(MainApp).mount('#main_app');
